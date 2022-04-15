a695a5bf569319d8996e946f2a5321cce9e0a45e
Luciano Alarcon <luchialarcon123@gmail.com>
2022-04-15 18:02:40 -0300

    BaSP-2022: ex03-arrays

---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,32 @@
+var month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log("ex03-arrays-A: " + month[4] + " " + month[10]);
+
+var month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log("ex03-arrays-B: " + month.sort());
+
+var fruits = ["Banana", "Manzana", "Naranja", "Pera"];
+fruits.unshift("Mango")
+fruits.push("Sandia")
+
+console.log("ex03-arrays-C: " + fruits);
+
+var fruits = ["Banana", "Manzana", "Naranja", "Pera"];
+fruits.shift();
+fruits.pop();
+
+console.log("ex03-arrays-D: " + fruits);
+
+var fruits = ["Banana", "Manzana", "Naranja", "Pera"];
+fruits.reverse();
+
+console.log("ex03-arrays-E: " + fruits);
+
+var month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log("ex03-arrays-F: " + month.join(" - "));
+
+var month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log("ex03-arrays-F: " + month.slice(4, 11))
