583d9e31b55e51026b73f9fc79f07e803c827d58
Luciano Alarcon <luchialarcon123@gmail.com>
2022-04-26 16:45:35 -0300

    BaSP-w06-2022: Finish js to login and sign up

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var signButton = document.getElementById('sign-up');
     signButton.addEventListener('click', signClick);
+    getTodayDate();
     var divTxt = document.getElementsByClassName('sign-up');
     divTxtName = divTxt[0];
     nameInput = divTxtName.children[1];
@@ -8,10 +9,16 @@ window.onload = function () {
     lNameInput = divLName.children[1];
     divDni = divTxt[2];
     dniInput = divDni.children[1];
+    divDate = divTxt[3];
+    dateInput = divDate.children[1];
     divPhone = divTxt[4];
     phoneInput = divPhone.children[1];
+    divAddress = divTxt[5];
+    addressInput = divAddress.children[1];
     divLocate = divTxt[6];
     locateInput = divLocate.children[1];
+    divCode = divTxt[7];
+    codeInput = divCode.children[1];
     divEmail = divTxt[8];
     emailInput = divEmail.children[1];
     divPass = divTxt[9];
@@ -27,42 +34,70 @@ window.onload = function () {
     nameInput.onblur = function () {
         myBlur(nameInput, divTxtName);
     }
+
     lNameInput.onfocus = function(){
         myFocus(lNameInput, divLName);
     }
     lNameInput.onblur = function () {
         myBlur(lNameInput, divLName);
     }
+
     dniInput.onfocus = function(){
         myFocus(dniInput, divDni);
     }
     dniInput.onblur = function () {
         myBlur(dniInput, divDni);
     }
+
+    dateInput.onfocus = function(){
+        myFocus(dateInput, divDate);
+    }
+    dateInput.onblur = function () {
+        myBlur(dateInput, divDate);
+    }
+
     phoneInput.onfocus = function(){
         myFocus(phoneInput, divPhone);
     }
     phoneInput.onblur = function () {
         myBlur(phoneInput, divPhone);
     }
+
+    addressInput.onfocus = function(){
+        myFocus(addressInput, divAddress);
+    }
+    addressInput.onblur = function () {
+        myBlur(addressInput, divAddress);
+    }
+
     locateInput.onfocus = function(){
         myFocus(locateInput, divLocate);
     }
     locateInput.onblur = function () {
         myBlur(locateInput, divLocate);
     }
+
+    codeInput.onfocus = function(){
+        myFocus(codeInput, divCode);
+    }
+    codeInput.onblur = function () {
+        myBlur(codeInput, divCode);
+    }
+
     emailInput.onfocus = function(){
         myFocus(emailInput, divEmail);
     }
     emailInput.onblur = function () {
         myBlur(emailInput, divEmail);
     }
+
     passInput.onfocus = function(){
         myFocus(passInput, divPass);
     }
     passInput.onblur = function () {
         myBlur(passInput, divPass);
     }
+
     confPassInput.onfocus = function(){
         myFocus(confPassInput, divConfPass);
     }
@@ -83,22 +118,9 @@ function myBlur(input, divTxtBox){
     }
  }
 
-
-/*var nameInput = document.getElementById('name');
-var lNameInput = document.getElementById('l-name');
-var dniInput = document.getElementById('dni');
-var dateBirthInput = document.getElementById('date');
-var phoneInput = document.getElementById('phone');
-var addInput = document.getElementById('address');
-var locationInput = document.getElementById('locate');
-var codeInput = document.getElementById('code');
-var emailInput = document.getElementById('email');
-var passInput = document.getElementById('password');
-var confPassInput = document.getElementById('conf-password');*/
-
 function signClick() {
     var textBoxes = document.getElementsByClassName('sign-up');
-    console.log(textBoxes)
+    //console.log(textBoxes)
     var data = {
         nameValue : document.getElementById('name').value,
         lastName : document.getElementById('l-name').value,
@@ -125,12 +147,24 @@ function signClick() {
         alert('ERROR\n DNI is invalid')
         allIsValid = false
     }
+    if (!isFullAge(data.birthDate, textBoxes[3])) {
+        alert('ERROR\nDate of Birth is invalid. You must have more than 18 years')
+        allIsValid = false;
+    }
     if (!validatePhone(data.phone, textBoxes[4])) {
-        alert('ERROR\n Phone is invalid')
+        alert('ERROR\nPhone is invalid')
+        allIsValid = false;
+    }
+    if (!validateAddress(data.address, textBoxes[5])) {
+        alert('ERROR\nThe address is invalid');
         allIsValid = false;
     }
     if (!validateLocation(data.location, textBoxes[6])) {
         alert('ERROR\nLocation is invalid')
+        allIsValid = false;
+    }
+    if (!validateCode(data.code, textBoxes[7])) {
+        alert('ERROR\nArea Code is invalid')
         allIsValid = false;
     }
     if (!validateEmail(data.email, textBoxes[8])) {
@@ -144,6 +178,12 @@ function signClick() {
     if (!validatePassword2(data.password, data.confPassword, textBoxes[10])) {
         alert('ERROR\nPassword do not match')
         allIsValid = false;
+    }
+    if (allIsValid) {
+        alert('¡Register succesfully!\nName: ' + data.nameValue + '\nLast Name: ' + data.lastName + 
+        '\nDNI: ' + data.dni + '\nDate of Birth: ' + data.birthDate + '\nPhone Number: ' + data.phone + 
+        '\nAddress: ' + data.address + '\nLocation: ' + data.location + '\nArea Code: ' + data.code + 
+        '\nEmail: ' + data.email + '\nPassword: ' + data.password + '\nConfirm Password: ' + data.confPassword);
     }
 }
 
@@ -183,11 +223,39 @@ function validateDNI(dni, txtBoxes){
     }
 }
 
+function getTodayDate(){
+    var dateInput = document.getElementById('date');
+    var currentDate = new Date();
+    var day = currentDate.getDate().toString();
+    var month = currentDate.getMonth();
+    month++;
+    if (month < 10) {
+        month = '0' + month;
+    }
+    month = month.toString();
+    var year = currentDate.getFullYear().toString();
+    var maxValue = year + '-' + (month) + '-' + day;
+    dateInput.setAttribute('max', maxValue);
+}
+
+function isFullAge(date, txtBoxes){
+    var dateInput = new Date(date);
+    var thisMoment = new Date(Date.now());
+    var isMajor = new Date(thisMoment - dateInput).getFullYear() - 1970 >= 18;
+    if (isMajor) {
+        hError(txtBoxes);
+        return true;
+    }else{
+        sError(txtBoxes);
+        return false;
+    }
+}
+
 function validatePhone(phone, txtBoxes){
     var nums = ['0','1','2','3','4','5','6','7','8','9'];
     var isValid = true;
     for(i = 0; i < phone.length; i++){
-        if (!numbers.includes(phone[i])) {
+        if (!nums.includes(phone[i])) {
             isValid = false;
         }
     }
@@ -200,6 +268,37 @@ function validatePhone(phone, txtBoxes){
     }
 }
 
+function validateAddress(address, txtBoxes){
+    var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var space = ' ';
+    var isValid = true;
+    var char = [];
+    for (i = 0; i < address.length; i++) {
+        if (letters.includes(address[i])) {
+            char.push('l');
+            if (i !== 0 && char[(i-1)] === 'n') {
+                isValid = false;
+            }
+        } else if (space === address[i]) {
+            char.push('s');
+        } else if (numbers.includes(address[i])) {
+            char.push('n');
+            if (i !== 0 && char[(i-1)] === 'l') {
+                isValid = false;
+            }
+        }
+    }
+    if(address.length >= 5 && isValid){
+        hError(txtBoxes);
+        return true;
+    } else{
+        sError(txtBoxes);
+        return false;
+    }
+}
+
+
 function validateLocation(locate, txtBoxes){
     var nums = ['0','1','2','3','4','5','6','7','8','9'];
     var minLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "l", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -210,8 +309,8 @@ function validateLocation(locate, txtBoxes){
     var lettMayusCount = 0;
     var special = false;
     for (i = 0; i < locateLower.length; i++){
-        if (number.includes(locateLower[i])) {
-            num++;
+        if (nums.includes(locateLower[i])) {
+            numCount++;
         }else if (minLetters.includes(locateLower[i])) {
             lettMinCount++;
         }else if (mayusLetters.includes(locateLower[i])) {
@@ -229,6 +328,23 @@ function validateLocation(locate, txtBoxes){
     }
 }
 
+function validateCode(code, txtBoxes){
+    var nums = ['0','1','2','3','4','5','6','7','8','9'];
+    var isValid = true;
+    for (i = 0; i < code.length; i++) {
+        if (!nums.includes(code[i])) {
+            isValid = false;
+        } 
+    }
+    if (code.length >= 4 && code.length <= 5 && isValid) {
+        hError(txtBoxes);
+        return true;
+    }else {
+        sError(txtBoxes);
+        return false
+    }
+}
+
 function validateEmail(email, txtBoxes){
     var eRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
     if (!eRegex.test(email)) {
@@ -240,7 +356,7 @@ function validateEmail(email, txtBoxes){
     }
 }
 
-function validatePassword(password, txtBoxes) {
+function validatePassword(password) {
     var numbers = ['0','1','2','3','4','5','6','7','8','9'];
     var num = 0;
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "l", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -260,10 +376,8 @@ function validatePassword(password, txtBoxes) {
         }
     }
     if (password.length >= 8 && num >= 1 && char >=1 && !signs) {
-        hError(txtBoxes);
         return true;
     } else{
-        sError(txtBoxes);
         return false;
     }
 }
